963792a0131ddf9900cf81ba0f5239722de5f734
Nithin Raj V <146872871+nithinraj1999@users.noreply.github.com>
2025-04-11 22:44:09 +0530

    Delete dist directory

--- dist/interfaces/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandlingMiddleware = void 0;
-const errorHandlingMiddleware = (err, req, res) => {
-    // Log error details
-    console.log("hhhh", req.method);
-};
-exports.errorHandlingMiddleware = errorHandlingMiddleware;
