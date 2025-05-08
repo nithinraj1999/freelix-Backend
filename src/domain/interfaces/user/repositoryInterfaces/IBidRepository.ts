b2a84d6fca1462421070c26f81f071acb030eb99
nithinraj1999 <vnithinraj99@gmail.com>
2025-05-08 21:33:17 +0530

    code refacctored

--- src/domain/interfaces/user/repositoryInterfaces/IBidRepository.ts
+++ src/domain/interfaces/user/repositoryInterfaces/IBidRepository.ts
@@ -1,3 +1,5 @@
+import { Bid } from "../../../entities/bid"
+import { User } from "../../../entities/user"
 export interface IBidRepository{
-    allBids(jobId: string):Promise<any>
+    allBids(jobId: string):Promise<(Bid & { freelancerId: User })[]>
 }
